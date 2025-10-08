80bcfd8822fb6f50910a9bfc03a1de952d3bc181
Man Truong <mantruon@usc.edu>
2025-10-08 13:46:19 -0700

    created mydatastore

---
+++ mydatastore.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <set>
+#include <vector>
+
+#include "mydatastore.h"
+
+using namespace std;
+
+myDataStore::myDataStore () {
+
+}
---
+++ mydatastore.h
@@ -0,0 +1,16 @@
+#ifndef MYDATASTORE_H
+#define MYDATASTORE_H
+#include <string>
+#include <set>
+#include <vector>
+#include "datastore.h"
+
+class myDataStore {
+public:
+
+protected:
+	std::vector<User> users_;
+	
+};
+
+#endif
