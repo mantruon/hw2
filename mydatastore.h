91217152a7b82db66ebb4fc6d69a25da5c5de753
Man Truong <mantruon@usc.edu>
2025-10-09 07:58:02 -0700

    implemented a bit more of mydatastore

--- mydatastore.h
+++ mydatastore.h
@@ -3,13 +3,18 @@
 #include <string>
 #include <set>
 #include <vector>
+#include <map>
 #include "datastore.h"
 
 class myDataStore {
 public:
 
 protected:
-	std::vector<User> users_;
+	// make a set of vectors for easy find()
+	std::set<User> users_;
+	// then make a map of users and a vector containing their products
+	// want a vector since we know that duplicate items can exist >> not a set or map
+	std::map<User, std::vector<Product>> carts;
 	
 };
 
